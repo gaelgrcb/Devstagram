93f32e467601359269e3128d84c376d818a7f26f
Gael Garcia <gaelgarciabst@gmail.com>
2024-07-24 10:57:44 -0600

    Se creo formulario para Registar Usuarios

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,27 +11,31 @@
     <header class="p-5 border-b bg-white shadow">
 
         <div class="container mx-auto flex justify-between">
-        <h1 class="text-3xl font-black">
-            DevStagram
+        <h1 class="text-3xl font-[1000]" >
+            <a href="/">
+                DevStagram
+            </a>
         </h1>
 
         <nav class="flex gap-3 items-center">
-            <a class="font-bold uppercase text-gray-600" href=""">Login</a>
-            <a class="font-bold uppercase text-gray-600" href="">Crear Cuenta</a>
+            <a class="font-bold uppercase text-gray-600" href="">Login</a>
+            <a class="font-bold uppercase text-gray-600" href="/create-account">Crear Cuenta</a>
         </nav>
 
         </div>
     </header>
 
-    <main class="container mx-auto mt-10 ml-10">
-        <h2 class="font-black text-center text-3xl mb-10">
+    <main class="container mx-auto mt-10">
+        <h2 class="text-center text-3xl mb-10 font-[1000]">
             @yield('titulo')
         </h2>
         @yield('contenido')
     </main>
 
     <footer class="text-center p-5 text-gray-500 font-bold uppercase">
-        DevStagram -Todos los derechos reservados @php echo date('Y') @endphp
+        DevStagram -Todos los derechos reservados
+        {{now()->year}}
     </footer>
+
 </body>
 </html>
